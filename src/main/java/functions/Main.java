5d8716108a05b8bba2a8196aad9e4f55a9e00a94
DragosCiob <ciobanudragos80@gmail.com>
2022-06-13 12:55:36 +0300

    Functions

---
+++ src/main/java/functions/Main.java
@@ -0,0 +1,12 @@
+package functions;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SalesRepresentativeSorting instance = new SalesRepresentativeSorting();
+
+        System.out.println(Arrays.toString(instance.sortByRevenue()));
+    }
+}
